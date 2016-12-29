6b8f6e910c22016c3560cb3937b7b30d575912f5
Zopanix <zopanix@gmail.com>
2016-12-29 05:40:40 -0500

    Removing bootstrapped deletion

--- resources/entrypoint.sh
+++ resources/entrypoint.sh
@@ -16,10 +16,6 @@ else
   CHEF_BOOTSTRAP=false
 fi
 ###
-# Deleting bootstrapped file
-###
-rm -f /var/opt/opscode/bootstrapped
-###
 # Launch chef-server 
 ###
 /opt/opscode/embedded/bin/ruby init.rb > /tmp/chef.log &
