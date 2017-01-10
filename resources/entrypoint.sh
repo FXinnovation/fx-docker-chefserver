63f71a19596fc8d9e1bfcd0fc9a1d3f2cb1ed20a
Zopanix <zopanix@gmail.com>
2017-01-10 12:41:32 -0500

    Corrected entrypoint
    The shortname of the organization wasn't setup properly so I changed that
    
    Also made some minor cosmetics changes on the entrypoint

--- resources/entrypoint.sh
+++ resources/entrypoint.sh
@@ -40,25 +40,24 @@ if [ $CHEF_BOOTSTRAP ]; then
     --filename /var/opt/opscode/$CHEF_USER.pem
   echo "### Creating Organsation"
   chef-server-ctl org-create \
-    short_name "$CHEF_ORGANISATION" \
+    --short_name "$CHEF_ORGANISATION" \
     --association_user $CHEF_USER \
     --filename /var/opt/opscode/$CHEF_ORGANISATION.pem
   ###
   # Displaying information
   ###
   echo "############################################################"
-  echo "###                     Information                     ### "
+  echo "###                     Information                      ###"
   echo "############################################################"
-  echo "# Chef User : $CHEF_USER                                   #"
-  echo "# User Password : $CHEF_PASSWORD                           #"
-  echo "# User Validator :                                         #"
-  cat /var/opt/opscode/$CHEF_USER.pem
-  echo "# Chef Organization : $CHEF_ORGANISATION                   #"
-  echo "#                                                          #"
-  echo "# Organization Validator:                                  #"
-  cat /var/opt/opscode/$CHEF_ORGANISATION.pem
+  echo "# Chef User : $CHEF_USER"
+  echo "# User Password : $CHEF_PASSWORD"
+  echo "# User Validator :"
+  echo "$(cat /var/opt/opscode/$CHEF_USER.pem)"
+  echo "# Chef Organization : $CHEF_ORGANISATION"
+  echo "# Organization Validator:"
+  echo "$(cat /var/opt/opscode/$CHEF_ORGANISATION.pem)"
   echo "############################################################"
-  echo "###                         End                         ### "
+  echo "###                         End                          ###"
   echo "############################################################"
 fi
 ###
